9a93488a0b3e560f25f88331951e5ad4574c7314
mmm25002500 <marryhan25648@gmail.com>
2023-02-14 17:56:02 +0800

    Fix: NativeWind error

--- App.tsx
+++ App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import {View} from 'react-native';
 import Nav from './src/components/Nav';
 import Home from './src/screens/Home';
-import {styled} from 'nativewind';
+// import {styled} from 'nativewind';
+// import {Text} from 'react-native/Libraries/Text/Text';
 // import HomeScreen from './src/screens/IconScreen';
-const StyledView = styled(View);
+// const StyledView = styled(View);
+// const StyledText = styled(Text);
 
 const App = () => {
   return (
-    <StyledView>
-      <StyledText className="text-center text-red-500">just red</StyledText>
+    <View>
+      {/* <Text className="text-center text-red-500">just red</Text> */}
       <Nav />
       <Home />
-    </StyledView>
+    </View>
   );
 };
 export default App;

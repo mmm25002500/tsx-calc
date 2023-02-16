2bd4c8088fbb96ba57b4c6b0ef3f5ff958e57d01
mmm25002500 <marryhan25648@gmail.com>
2023-02-16 17:44:18 +0800

    Feat: add radio btn

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -7,31 +7,54 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import {RadioButton, Text as PaperText} from 'react-native-paper';
 
 // Tailwind CSS Components
 const StyledView = styled(View);
 const StyledText = styled(Text);
+const StyledTextInput = styled(TextInput);
 
 const TextInputExample = ({onChangeText}: any) => {
   return (
     <SafeAreaView>
-      <TextInput onChangeText={onChangeText} />
+      <StyledTextInput
+        onChangeText={onChangeText}
+        className="text-red-500 text-end"
+      />
     </SafeAreaView>
   );
 };
 
 const Home = () => {
   const [text, onChangeText] = useState('');
+  const [value, setValue] = React.useState('first');
 
   return (
     <StyledView className="mt-10">
-      <StyledText className="text-center text-red-500">just red</StyledText>
+      <StyledView className="border-2 border-cyan-400">
+        <StyledText className="text-center text-lg text-red-500">
+          just red
+        </StyledText>
+      </StyledView>
 
       <TextInputExample onChangeText={onChangeText} />
 
       <TouchableOpacity onPress={() => console.log(text)}>
-        <Text style={{color: 'cyan'}}>{text}</Text>
+        <StyledText className="text-red-500">{text}</StyledText>
       </TouchableOpacity>
+
+      <RadioButton.Group
+        onValueChange={newValue => setValue(newValue)}
+        value={value}>
+        <StyledView style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={{marginRight: 10}}>First</Text>
+          <RadioButton value="first" />
+        </StyledView>
+        <StyledView style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Text style={{marginRight: 10}}>Second</Text>
+          <RadioButton value="second" />
+        </StyledView>
+      </RadioButton.Group>
     </StyledView>
   );
 };
